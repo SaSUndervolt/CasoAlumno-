f8222a6892116bf5ca620084b2c49a82b6ede1d9
lalit <lalit@DESKTOP-VREQOAT.bbrouter>
2024-01-18 17:03:03 -0300

    CommitN1

---
+++ src/test/java/com/mycompany/java_crud_mysql/AlumnosTest.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.mycompany.java_crud_mysql;
+
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lalit
+ */
+public class AlumnosTest {
+    
+    @Test
+    public void testUno(){
+        
+        
+   
+    }
+    
+    
+    
+
+    /*@Test
+    public void testInsertarAlumno() {
+        System.out.println("InsertarAlumno");
+        JTextField paramNombres = null;
+        JTextField paramApellidos = null;
+        JTextField paramRut = null;
+        Alumnos instance = new Alumnos();
+        instance.InsertarAlumno(paramNombres, paramApellidos, paramRut);
+        fail("The test case is a prototype.");
+    }
+*/
+
+}
